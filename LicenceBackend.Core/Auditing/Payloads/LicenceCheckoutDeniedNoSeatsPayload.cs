9e3d228745e3f3af0482ae4bcf46081da0aab831
Kieran Ramsey <ramseykieran@gmail.com>
2026-05-18 22:15:01 -0600

    enrich denial payload with member id and hwid base64 for investigation parity

--- LicenceBackend.Core/Auditing/Payloads/LicenceCheckoutDeniedNoSeatsPayload.cs
+++ LicenceBackend.Core/Auditing/Payloads/LicenceCheckoutDeniedNoSeatsPayload.cs
@@ -2,6 +2,8 @@ namespace LicenceBackend.Core.Auditing.Payloads;
 
 public sealed record LicenceCheckoutDeniedNoSeatsPayload(
     string InstanceIdHashPrefix,
+    Guid? MemberUserId,
+    string? HwidHmacBase64,
     string SourceIp,
     int ActiveSeats,
     int MaxSeats,
