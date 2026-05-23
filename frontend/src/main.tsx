6d0358b1c43f42d178428e382de0f2b8881110d7
Kieran Ramsey <ramseykieran@gmail.com>
2026-05-22 21:58:59 -0600

    swap to slate palette and drop fraunces

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,10 +6,10 @@ import { createRoot } from 'react-dom/client';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
-import '@fontsource/fraunces/400.css';
-import '@fontsource/fraunces/600.css';
+import '@fontsource/inter/700.css';
 import '@fontsource/jetbrains-mono/400.css';
 import '@fontsource/jetbrains-mono/500.css';
+import '@fontsource/jetbrains-mono/600.css';
 
 import './index.css';
 import './theme/theme-store';
